eff7906c97785685bec6f320641f88f907186e0d
verla-lisby <lisbyverla69@gmial.com>
2025-03-22 13:28:08 +0400

    first commit

---
+++ Phonebook/Phonebook.Designer.cs
@@ -0,0 +1,290 @@
+﻿namespace Phonebook
+{
+    partial class Phonebook
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Phonebook));
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.btnCreate = new System.Windows.Forms.Button();
+            this.dgvPhonebook = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtTel = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtEmail = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtAddress = new System.Windows.Forms.TextBox();
+            this.txtSearch = new System.Windows.Forms.TextBox();
+            this.btnSearch = new System.Windows.Forms.Button();
+            this.btnUpdate = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.btnClearAll = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.lblID = new System.Windows.Forms.Label();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.aboutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvPhonebook)).BeginInit();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(12, 32);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(318, 20);
+            this.txtName.TabIndex = 1;
+            // 
+            // btnCreate
+            // 
+            this.btnCreate.Location = new System.Drawing.Point(12, 230);
+            this.btnCreate.Name = "btnCreate";
+            this.btnCreate.Size = new System.Drawing.Size(75, 23);
+            this.btnCreate.TabIndex = 5;
+            this.btnCreate.Text = "Create";
+            this.btnCreate.UseVisualStyleBackColor = true;
+            this.btnCreate.Click += new System.EventHandler(this.btnCreate_Click);
+            // 
+            // dgvPhonebook
+            // 
+            this.dgvPhonebook.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvPhonebook.Location = new System.Drawing.Point(336, 38);
+            this.dgvPhonebook.Name = "dgvPhonebook";
+            this.dgvPhonebook.ReadOnly = true;
+            this.dgvPhonebook.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dgvPhonebook.Size = new System.Drawing.Size(270, 215);
+            this.dgvPhonebook.TabIndex = 11;
+            this.dgvPhonebook.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgvPhonebook_CellContentClick);
+            this.dgvPhonebook.CellDoubleClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgvPhonebook_CellDoubleClick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 16);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Name";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 69);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(22, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Tel";
+            // 
+            // txtTel
+            // 
+            this.txtTel.Location = new System.Drawing.Point(12, 85);
+            this.txtTel.Name = "txtTel";
+            this.txtTel.Size = new System.Drawing.Size(318, 20);
+            this.txtTel.TabIndex = 2;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 130);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(32, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Email";
+            // 
+            // txtEmail
+            // 
+            this.txtEmail.Location = new System.Drawing.Point(12, 146);
+            this.txtEmail.Name = "txtEmail";
+            this.txtEmail.Size = new System.Drawing.Size(318, 20);
+            this.txtEmail.TabIndex = 3;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 179);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(45, 13);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Address";
+            // 
+            // txtAddress
+            // 
+            this.txtAddress.Location = new System.Drawing.Point(12, 195);
+            this.txtAddress.Name = "txtAddress";
+            this.txtAddress.Size = new System.Drawing.Size(318, 20);
+            this.txtAddress.TabIndex = 4;
+            // 
+            // txtSearch
+            // 
+            this.txtSearch.Location = new System.Drawing.Point(356, 9);
+            this.txtSearch.Name = "txtSearch";
+            this.txtSearch.Size = new System.Drawing.Size(141, 20);
+            this.txtSearch.TabIndex = 9;
+            // 
+            // btnSearch
+            // 
+            this.btnSearch.Location = new System.Drawing.Point(503, 9);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(75, 23);
+            this.btnSearch.TabIndex = 10;
+            this.btnSearch.Text = "Search";
+            this.btnSearch.UseVisualStyleBackColor = true;
+            this.btnSearch.Click += new System.EventHandler(this.btnSearch_Click);
+            // 
+            // btnUpdate
+            // 
+            this.btnUpdate.Location = new System.Drawing.Point(174, 230);
+            this.btnUpdate.Name = "btnUpdate";
+            this.btnUpdate.Size = new System.Drawing.Size(75, 23);
+            this.btnUpdate.TabIndex = 7;
+            this.btnUpdate.Text = "Update";
+            this.btnUpdate.UseVisualStyleBackColor = true;
+            this.btnUpdate.Click += new System.EventHandler(this.btnUpdate_Click);
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Location = new System.Drawing.Point(93, 230);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(75, 23);
+            this.btnDelete.TabIndex = 6;
+            this.btnDelete.Text = "Delete";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // btnClearAll
+            // 
+            this.btnClearAll.Location = new System.Drawing.Point(255, 230);
+            this.btnClearAll.Name = "btnClearAll";
+            this.btnClearAll.Size = new System.Drawing.Size(75, 23);
+            this.btnClearAll.TabIndex = 8;
+            this.btnClearAll.Text = "Clear All";
+            this.btnClearAll.UseVisualStyleBackColor = true;
+            this.btnClearAll.Click += new System.EventHandler(this.btnClearAll_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(76, 12);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(18, 13);
+            this.label5.TabIndex = 12;
+            this.label5.Text = "ID";
+            // 
+            // lblID
+            // 
+            this.lblID.AutoSize = true;
+            this.lblID.Location = new System.Drawing.Point(76, 9);
+            this.lblID.Name = "lblID";
+            this.lblID.Size = new System.Drawing.Size(0, 13);
+            this.lblID.TabIndex = 13;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.aboutToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(629, 24);
+            this.menuStrip1.TabIndex = 14;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // aboutToolStripMenuItem
+            // 
+            this.aboutToolStripMenuItem.Name = "aboutToolStripMenuItem";
+            this.aboutToolStripMenuItem.Size = new System.Drawing.Size(52, 20);
+            this.aboutToolStripMenuItem.Text = "About";
+            this.aboutToolStripMenuItem.Click += new System.EventHandler(this.aboutToolStripMenuItem_Click);
+            // 
+            // Phonebook
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.ClientSize = new System.Drawing.Size(629, 273);
+            this.Controls.Add(this.lblID);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.btnClearAll);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.btnUpdate);
+            this.Controls.Add(this.btnSearch);
+            this.Controls.Add(this.txtSearch);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtAddress);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtEmail);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtTel);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dgvPhonebook);
+            this.Controls.Add(this.btnCreate);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.menuStrip1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MainMenuStrip = this.menuStrip1;
+            this.MaximizeBox = false;
+            this.Name = "Phonebook";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Phonebook";
+            this.Load += new System.EventHandler(this.Phonebook_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvPhonebook)).EndInit();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        private void Phonebook_Load(object sender, System.EventArgs e)
+        {
+            ClearAll();
+            loadData();
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.Button btnCreate;
+        private System.Windows.Forms.DataGridView dgvPhonebook;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtTel;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtEmail;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtAddress;
+        private System.Windows.Forms.TextBox txtSearch;
+        private System.Windows.Forms.Button btnSearch;
+        private System.Windows.Forms.Button btnUpdate;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.Button btnClearAll;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label lblID;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem aboutToolStripMenuItem;
+    }
+}
+
---
+++ Phonebook/Phonebook.cs
@@ -0,0 +1,211 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace Phonebook
+{
+    public partial class Phonebook : Form
+    {
+        public Phonebook()
+        {
+            InitializeComponent();
+        }
+        string ConString = @"Data Source=.\SQLSERVER2014;Initial Catalog=PhonebookDB;Integrated Security=True";
+        SqlConnection con = new SqlConnection();
+        SqlCommand cmd = new SqlCommand();
+        SqlDataAdapter da = new SqlDataAdapter();
+        SqlDataReader dr;
+        DataTable dt = new DataTable();
+
+
+
+        void ClearAll()
+        {
+            txtName.Clear();
+            txtEmail.Clear();
+            txtTel.Clear();
+            txtAddress.Clear();
+            txtName.Focus();
+            lblID.Text = "";
+            txtSearch.Clear();
+
+        }
+
+        object loadData()
+        {
+            String query = "Select * From tblPhonebook ";
+            con = new SqlConnection(ConString);
+            con.Open();
+            cmd = new SqlCommand(query, con);
+            da = new SqlDataAdapter(cmd);
+            dt = new DataTable();
+            da.Fill(dt);
+            if (dt.Rows.Count > 0)
+            {
+                return dgvPhonebook.DataSource = dt;
+
+            }
+            else
+            {
+                dt = null;
+                return dt;
+            }
+        }
+
+
+        private void dgvPhonebook_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            ClearAll();
+            loadData();
+        }
+
+        private void btnCreate_Click(object sender, EventArgs e)
+        {
+            if (txtName.Text == "" || txtEmail.Text == "" || txtTel.Text == "" || txtAddress.Text == "")
+            {
+                MessageBox.Show("Null parameters", "Status Update", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            else
+            {
+                string query = "Insert into tblPhonebook(Name, Tel, Email, Address) values('" + txtName.Text
+                    + "', '" + txtTel.Text + "', '" + txtEmail.Text + "', '" + txtAddress.Text + "')";
+                con = new SqlConnection(ConString);
+                con.Open();
+                cmd = new SqlCommand(query, con);
+                int i = cmd.ExecuteNonQuery();
+                if (i > 0)
+                {
+                    MessageBox.Show("Insert Successful", "Status Update", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+                else
+                {
+                    MessageBox.Show("Insert Failed", "Status Update", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+                ClearAll();
+                loadData();
+            }
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            if (lblID.Text == "")
+            {
+                MessageBox.Show("Null parameters", "Status Update", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            else
+            {
+                string query = "Delete from tblPhonebook where PhonebookID = '" + lblID.Text + "'";
+                con = new SqlConnection(ConString);
+                con.Open();
+                cmd = new SqlCommand(query, con);
+                int i = cmd.ExecuteNonQuery();
+                if (i > 0)
+                {
+                    MessageBox.Show("Delete Successful", "Status Update", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+                else
+                {
+                    MessageBox.Show("Delete Failed", "Status Update", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+                ClearAll();
+                loadData();
+            }
+        }
+
+        private void dgvPhonebook_CellDoubleClick(object sender, DataGridViewCellEventArgs e)
+        {
+            lblID.Text = dgvPhonebook.SelectedRows[0].Cells[0].Value.ToString();
+            txtName.Text = dgvPhonebook.SelectedRows[0].Cells[1].Value.ToString();
+            txtTel.Text = dgvPhonebook.SelectedRows[0].Cells[2].Value.ToString();
+            txtEmail.Text = dgvPhonebook.SelectedRows[0].Cells[3].Value.ToString();
+            txtAddress.Text = dgvPhonebook.SelectedRows[0].Cells[4].Value.ToString();
+        }
+
+        private void btnClearAll_Click(object sender, EventArgs e)
+        {
+            ClearAll();
+        }
+
+        private void btnUpdate_Click(object sender, EventArgs e)
+        {
+
+            if (lblID.Text == "")
+            {
+                MessageBox.Show("Null parameters", "Status Update", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            else
+            {
+                string query = "Update tblPhonebook set Name ='" + txtName.Text
+                    + "', Tel = '" + txtTel.Text + "', Email ='" + txtEmail.Text + "', Address = '" + txtAddress.Text + "' where PhonebookID = '" + lblID.Text + "'";
+                con = new SqlConnection(ConString);
+                con.Open();
+                cmd = new SqlCommand(query, con);
+                int i = cmd.ExecuteNonQuery();
+                if (i > 0)
+                {
+                    MessageBox.Show("Update Successful", "Status Update", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+                else
+                {
+                    MessageBox.Show("Update Failed", "Status Update", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+                ClearAll();
+                loadData();
+            }
+        }
+
+        private void btnSearch_Click(object sender, EventArgs e)
+        {
+            if (txtSearch.Text == "")
+            {
+                MessageBox.Show("Null parameters", "Status Update", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            else
+            {
+                string query = "Select * From tblPhonebook Where Name = '" + txtSearch.Text + "' ";
+                con = new SqlConnection(ConString);
+                con.Open();
+                cmd = new SqlCommand(query, con);
+                da = new SqlDataAdapter(cmd);
+                dt = new DataTable();
+                da.Fill(dt);
+                if (dt.Rows.Count > 0)
+                {
+                    dgvPhonebook.DataSource = dt;
+                    MessageBox.Show("Search Found", "Status Update", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+                else
+                {
+                    loadData();
+                    MessageBox.Show("Search Not Found", "Status Update", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+                }
+            }
+        }
+
+        private void aboutToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            About abt = new About();
+            abt.ShowDialog();
+        }
+    }
+
+}
+            
+
+    
+
+
+
+
+
+
+
+
